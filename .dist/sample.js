2d4830711f3a6440b565fe3a401838fa25e597ec
Lakshmana18 <lakshmanannamneedi143@gmail.com>
2024-08-02 21:12:47 +0530

    test-2

---
+++ .dist/sample.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName("lachu")[0].textContent="Lakshman"
+document.getElementById("lachu").textContent="From CSE 2/4"
+document.querySelector("#lachu").textContent="Learning JS"
+document.querySelector("lachu").shadowRoot
