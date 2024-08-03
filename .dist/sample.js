21e16735e064057d324a52f3b13deb181ab4ecca
Lakshmana18 <lakshmanannamneedi143@gmail.com>
2024-08-03 16:47:11 +0530

    3/8

--- .dist/sample.js
+++ .dist/sample.js
@@ -1,4 +1,3 @@
 document.getElementsByClassName("lachu")[0].textContent="Lakshman"
 document.getElementById("lachu").textContent="From CSE 2/4"
 document.querySelector("#lachu").textContent="Learning JS"
-document.querySelector("lachu").shadowRoot
---
+++ datatypes/datatypes.js
@@ -0,0 +1,35 @@
+let arr={name:"kumar"}
+   // console.log(arr)
+
+    var data={
+        name:"Lakshman",
+        branch: "cse",
+        college: "SRKREC"
+    }
+  //  console.log(data)
+    var data={
+        name:"Lakshman123",
+       branch: "cse",
+        college: "SRKREC"
+    }
+    //console.log(data.branch||"no data")
+    var data={
+        name:"Lakshman123",
+       branch: "cse",
+        college: "SRKREC"
+    }
+   // console.log(data.branch&&"no data")
+    var data={
+        name:"Lakshman123",
+       branch: "cse",
+        college: "SRKREC"
+    }
+   // console.log(data.branch??"no data")
+
+
+   var data={
+    name:"Lakshman123",
+   branch: "cse",
+    college: "SRKREC"
+}
+//console.log(data.branch!!"no data")
